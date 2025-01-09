f58c9e3a320919b42c6a64c6b2886617bc1a6b82
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-01-09 12:53:55 +0530

    create react app using vite

---
+++ 01_vite/src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from "./Hello.jsx";
+
+function App() {
+  return (
+    <>
+    <Hello />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nulla error nisi velit atque, omnis eum impedit assumenda dignissimos veritatis voluptatum temporibus rem nesciunt. Iste velit excepturi eius error suscipit.</p>
+    </>
+  ) 
+    
+}
+
+export default App;
